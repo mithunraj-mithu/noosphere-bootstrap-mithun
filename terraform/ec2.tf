346006044a70f9eeed62ceeb08d173c71be150a2
Mithun Raj <mithunraj@gmail.com>
2026-02-07 14:24:03 +0530

    feat/terraform: added ec2.tf

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -10,3 +10,19 @@ data "aws_ami" "amazon_linux" {
   }
 }
 
+
+resource "aws_instance" "server" {
+  for_each = var.instances
+  ami      = data.aws_ami.amazon_linux.id
+
+  instance_type = each.value.instance_type
+
+  tags = merge(var.tags,
+
+    {
+      Name = "${each.key.value.name}-${var.environment}"
+
+    }
+  )
+
+}
