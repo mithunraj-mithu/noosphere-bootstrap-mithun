8a70f4f85f4eee32bcd7771c8a1e77560c8a6bb7
Mithun Raj <mithunraj@gmail.com>
2026-02-07 12:45:22 +0530

    feat: updating the variable.tf file to add vpc and ec2 variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -43,7 +43,44 @@ variable "instances" {
   description = "Map of EC2 instance configurations"
   type = map(object({
     instance_type = string
-    name          = string
+    #name          = string
   }))
   default = {}
 }
+
+# VPC variables
+
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+
+  validation {
+    condition     = can(cidrhost(var.vpc_cidr, 0))
+    error_message = "VPC CIDR must be a valid IPv4 CIDR block."
+  }
+}
+
+variable "public_subnet" {
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  description = "public subnet"
+}
+
+
+
+#Common tags for resources
+
+variable "tags" {
+
+  default = {
+    Environment = "Dev"
+    Squad       = "DevOps"
+    Created_by  = "terraform"
+
+  }
+
+}
+
+
+
