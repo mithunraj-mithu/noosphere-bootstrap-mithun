4cada1a821367e599ecdf1b686866f69db9e599d
Mithun Raj <mithunraj@gmail.com>
2026-02-09 06:20:17 +0530

    feat:(terraform) corrected ec2 instance type login

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -3,6 +3,6 @@ resource "aws_instance" "server" {
 
   for_each = var.instances
   ami = data.aws_ami.amazon_linux
-  instance_type = each.key.instance_type
+  instance_type = each.value.instance_type
   
 }
