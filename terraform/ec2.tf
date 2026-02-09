fb8e4e1ce0c84e199b89a66e6b82778658ea0de7
Mithun Raj <mithunraj@gmail.com>
2026-02-09 06:10:47 +0530

    feat:(terraform) updated ec2 instance block and spec in dev.tfvars file

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,12 +1,8 @@
-# EC2 Instances
-# Data source to get latest Amazon Linux 2 AMI
-data "aws_ami" "amazon_linux" {
-  most_recent = true
-  owners      = ["amazon"]
 
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
-  }
+resource "aws_instance" "server" {
+
+  for_each = var.instances
+  ami = data.aws_ami.amazon_linux
+  instance_type = each.key.instance_type
+  
 }
-
